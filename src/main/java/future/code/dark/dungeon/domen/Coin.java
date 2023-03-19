280d05fd4cc35ced516c80107b7d88958703b068
ArtemLebedev02 <98605409+ArtemLebedev02@users.noreply.github.com>
2023-03-19 14:37:11 +0300

    Update Coin.java

--- src/main/java/future/code/dark/dungeon/domen/Coin.java
+++ src/main/java/future/code/dark/dungeon/domen/Coin.java
@@ -2,10 +2,19 @@ package future.code.dark.dungeon.domen;
 
 import future.code.dark.dungeon.config.Configuration;
 
-public class Coin extends GameObject{
+import javax.xml.stream.Location;
+
+public class Coin extends GameObject {
+    boolean coinCol;
+    public boolean getCoinCol() {
+        return this.coinCol;
+    }
+    public void setCoinCol(boolean coinCol) {
+        this.coinCol = coinCol;
+    }
 
     public Coin(int xPosition, int yPosition) {
         super(xPosition, yPosition, Configuration.COIN_SPRITE);
     }
-
+    int collectibles = 0;
 }
